07409bbf748673b0edc63330e193fe5bfeab6c53
Igor Berlenko <igor@berlenko.com>
2018-11-01 07:45:02 +0300

    init

---
+++ assets/js/scripts.js
@@ -0,0 +1,24 @@
+/**
+ * Swap Explorer
+ */
+
+(function( $ ){
+	"use strict";
+
+	/**
+	 * Name
+	 */
+	$(document).on('click', 'button', function(){
+		
+		
+	});
+
+	/**
+	 * Tooltip
+	 */
+	$('.difference-help').tooltip({
+		placement: 'bottom',
+		template: '<div class="tooltip swap-tooltip" role="tooltip"><div class="arrow"></div><div class="tooltip-inner"></div></div>',
+	});
+
+})( jQuery );
