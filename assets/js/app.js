627721bf53011072c4bf4b43433babb4d064235b
sevenflash <sevenflash@yandex.ru>
2019-01-18 01:02:37 +0200

    find swap by secret hash

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,5 @@
+var baseURL = 'https://seven.swap.online/swaps-testnet/state'
+
 var model = {
   swaps: [
     {
@@ -32,18 +34,20 @@ var model = {
         withdrawTx: '0xb55107996afd0565699483e19db80996473474e2aa75'
       }
     }
-  ]
+  ],
+  currentSwap: {}
 }
 
 var app = {
   init: function() {
     historyView.init()
+    currentSwapView.init()
 
     this.updateSwaps()
   },
 
   updateSwaps: function() {
-    $.getJSON('https://seven.swap.online/swaps-testnet/state/ethbtc', function(data) {
+    $.getJSON(baseURL + '/ethbtc', function(data) {
       model.swaps = data.map((item) => {
 	item.alice.value = item.alice.value / 10**18
         item.bob.value = item.bob.value / 10**8
@@ -57,11 +61,60 @@ var app = {
 
   getSwaps: function() {
     return model.swaps
+  },
+
+  getCurrentSwap: function() {
+    return model.currentSwap
+  },
+
+  fetchSwapRaw: function(secretHash) {
+    $.getJSON(baseURL + '/swap/' + secretHash, function(data) {
+      model.currentSwap = data
+
+      currentSwapView.render()
+    })
+  }
+}
+
+var currentSwapView = {
+  init: function() {
+    $('#secretHashField').on('input', function(e) {
+      var secretHash = e.currentTarget.value
+
+      app.fetchSwapRaw(secretHash)
+    })
+
+    this.currentSwapElem = $('#currentSwapInfo')
+
+    var secretHash = $('#secretHashField').val()
+
+    app.fetchSwapRaw(secretHash)
+
+    this.render()
+  },
+
+  render: function() {
+    var currentSwapInfo = app.getCurrentSwap()
+
+    this.currentSwapElem.html(JSON.stringify(currentSwapInfo, null, 2))
   }
 }
 
 var historyView = {
   init: function() {
+    $.views.converters("statusToColor", function(status) {
+      switch (status) {
+        case 'waiting':
+          return 'rgb(255,255,51,0.5)';
+
+        case 'success':
+          return 'rgb(0,179,0,0.5)';
+
+        default:
+          return 'rgb(255,51,51,0.5)';
+      }
+    });
+
     this.historyElem = $('.history-list')
 
     this.template = $.templates('#history-list-item')
